17fbfbd3b29a538491b53944673d7e46234c4637
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-14 22:26:03 +0200

    Add files via upload

---
+++ Programming Fundamentals/Debugging and Methods/1-Умножаване на четни по нечетни/Умножаване на четни по нечетни.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _1_Умножаване_на_четни_по_нечетни
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMultiplyOfSumOfEvenNumbersAndSumOfEvenNumbers(number));
+        }
+        static int GetsumOfEvenNumbers(int number)
+        {
+            int evenSum = 0;
+            if (number > 0)
+            {
+                while (number > 0)
+                {
+                    int ost = number % 10;
+                    if (ost % 2 == 0)
+                    {
+                        evenSum += ost;
+                    }
+                    number /= 10;
+                }
+            }
+            else
+            {
+                number *= -1;
+                while (number > 0)
+                {
+                    int ost = number % 10;
+                    if (ost % 2 == 0)
+                    {
+                        evenSum += ost;
+                    }
+                    number /= 10;
+                }
+            }
+            return evenSum;
+        }
+        static int GetsumOfOddNumbers(int number)
+        {
+            int oddSum = 0;
+            if (number>0)
+            {
+                while (number > 0)
+                {
+                    int ost = number % 10;
+                    if (ost % 2 == 1)
+                    {
+                        oddSum += ost;
+                    }
+                    number /= 10;
+                }
+            }
+            else
+            {
+                number *= -1;
+                while (number > 0)
+                {
+                    int ost = number % 10;
+                    if (ost % 2 == 1)
+                    {
+                        oddSum += ost;
+                    }
+                    number /= 10;
+                }
+            }
+            return oddSum;
+        }
+        static int GetMultiplyOfSumOfEvenNumbersAndSumOfEvenNumbers(int number)
+        {
+            int oddSums = GetsumOfOddNumbers(number);
+            int evenSums = GetsumOfEvenNumbers(number);
+            return oddSums * evenSums;
+        }
+    }
+}
---
+++ Programming Fundamentals/Debugging and Methods/2-Дебъгване на кода.Почивни дни между две дати/Дебъгване на кода.Почивни дни между две дати.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _2_Дебъгване_на_кода.Почивни_дни_между_две_дати
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(),
+           "d.M.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.M.yyyy", CultureInfo.InvariantCulture);
+            var holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date=date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
+                    date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                }
+            }
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
