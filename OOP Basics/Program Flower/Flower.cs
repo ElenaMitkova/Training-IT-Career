111e8360e627622e7aebf665347f97618cafa148
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 21:10:56 +0200

    Add files via upload

---
+++ OOP Basics/Program Flower/Flower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+namespace RegularExam
+{
+    internal class Flower
+    {
+        private string type;
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        private string color;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (price > 100)
+                {
+                    throw new ArgumentException("Invalid flower price!");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+        public Flower(string type, string color, double price)
+        {
+            Type = type;
+            Color = color;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Flower {Type} with color {Color} costs {Price:f2}";
+        }
+    }
+}
---
+++ OOP Basics/Program Flower/FlowerStore.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace RegularExam
+{
+    internal class FlowerStore
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length<6)
+                {
+                    throw new ArgumentException("Invalid flower store name!");
+                }
+                
+                    name = value;
+                
+            }
+        }
+        List<Flower> lflower;
+        public FlowerStore(string name)
+        {
+            Name = name;
+            lflower = new List<Flower>();
+        }
+        public void AddFlower(Flower flower)
+        {
+            lflower.Add(flower);
+        }
+        public bool SellFlower(Flower flower)
+        {
+            foreach (var item in lflower)
+            {
+                if (item.Type==flower.Type&&item.Color==flower.Color)
+                {
+                    lflower.Remove(item);
+                    return true;
+                    
+                }
+                
+            }
+            return
+                false;
+        }
+        public double CalculateTotalPrice()
+        {
+            return lflower.Sum(item => item.Price);
+        }
+        public void RenameFlowerStore(string newName)
+        {
+            if (newName.Length < 6)
+            {
+                throw new ArgumentException("Invalid flower store name!");
+            }
+            else
+            {
+                Name = newName;
+            }
+        }
+        public void SellAllFlowers()
+        {
+            lflower.Clear();
+        }
+        public Flower GetFlowerWithHighestPrice()
+        {
+            return lflower.OrderByDescending(x => x.Price).FirstOrDefault();
+        }
+        public double GetFlowerWithLowestPrice()
+        {
+            lflower.OrderByDescending(x => x.Price).FirstOrDefault();
+            return lflower[lflower.Count-1].Price;
+        }
+        public override string ToString()
+        {
+            if (lflower.Count > 0)
+            {
+                return $"Flower store {Name} has {lflower.Count} flower/s: \n" +
+                    $"{string.Join("\n", lflower)}";
+            }
+            else
+            {
+                return $"Flower store {Name} has no available flowers.";
+            }
+        }
+    }
+}
