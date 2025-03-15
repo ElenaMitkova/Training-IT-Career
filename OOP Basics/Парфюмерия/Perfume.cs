76f66af6d42054e9408f9d80c59606c928332cfd
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 21:13:18 +0200

    Add files via upload

---
+++ OOP Basics/Парфюмерия/Perfume.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace RegularExam
+{
+    internal class Perfume
+    {
+        private string brand;
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value>100)
+                {
+                    throw new ArgumentException("Invalid perfume price!");
+                }
+                price = value;
+            }
+        }
+        public Perfume(string brand, double price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Perfume {Brand} costs {Price:f2}";
+        }
+    }
+}
---
+++ OOP Basics/Парфюмерия/Perfumery.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace RegularExam
+{
+    internal class Perfumery
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length<6)
+                {
+                    throw new ArgumentException("Invalid perfumery name!");
+                }
+                name = value;
+            }
+        }
+        List<Perfume> perfumes;
+        public Perfumery(string name)
+        {
+            Name = name;
+            perfumes = new List<Perfume>();
+        }
+        public void AddPerfume(Perfume perfume)
+        {
+            perfumes.Add(perfume);
+        }
+        public bool SellPerfume(Perfume perfume)
+        {
+            foreach (var item in perfumes)
+            {
+                if (item.Brand == perfume.Brand && item.Price == perfume.Price)
+                {
+                    perfumes.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public double CalculateTotalPrice()
+        {
+            return perfumes.Sum(p => p.Price);
+        }
+        public void RenamePerfumery(string newName)
+        {
+            if (newName.Length < 6)
+            {
+                throw new ArgumentException("Invalid perfumery name!");
+            }
+            name = newName;
+        }
+        public void SellAllPerfumes()
+        {
+            perfumes.Clear();
+        }
+        public Perfume GetPerfumeWithHighestPrice()
+        {
+            return perfumes.OrderByDescending(p => p.Price).FirstOrDefault();
+        }
+        public Perfume GetPerfumeWithLowestPrice()
+        {
+            return perfumes.OrderBy(p => p.Price).FirstOrDefault();
+        }
+        public override string ToString()
+        {
+            if (perfumes.Count > 0)
+            {
+                return $"Perfumery {Name} has {perfumes.Count} perfume/s:\n" +
+                    $"{string.Join("\n", perfumes)}";
+            }
+            return $"Perfumery {Name} has no available perfumes.";
+        }
+    }
+}
