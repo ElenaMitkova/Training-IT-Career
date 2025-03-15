c6d66c48c89fad167b1f3843c1f0ea0e6fb54496
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 21:11:58 +0200

    Add files via upload

---
+++ OOP Basics/Магазин за шапки/Hat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegularExam
+{
+    internal class Hat
+    {
+        private string type;
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        private string color;
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set 
+            {
+                if (price >100)
+                {
+                    throw new ArgumentException("Invalid hat price!");
+                }
+                price = value;
+            }
+        }
+        public Hat(string type, string color, double price)
+        {
+            Type = type;
+            Color = color;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"Hat {Type} with color {Color} costs {Price}";
+        }
+    }
+}
---
+++ OOP Basics/Магазин за шапки/HatShop.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+namespace RegularExam
+{
+    internal class HatShop
+    {
+        List<Hat> hats;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length<6)
+                {
+                    throw new ArgumentException("Invalid hat shop name!");
+                }
+                name = value;
+            }
+        }
+        public HatShop(string name)
+        {
+            Name = name;
+            hats = new List<Hat>();
+        }
+        public void AddHat(Hat hat)
+        {
+            hats.Add(hat);
+        }
+        public bool SellHat(Hat hat)
+        {
+            foreach (var item in hats)
+            {
+                if (item.Type == hat.Type && item.Color == hat.Color)
+                {
+                    hats.Remove(hat);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public double CalculateTotalPrice()
+        {
+            return hats.Sum(hat => hat.Price);
+        }
+        public void RenameHatShop(string name)
+        {
+            if (name.Length < 6)
+            {
+                throw new ArgumentException("Invalid hat shop name!");
+            }
+            Name = name;
+        }
+        public void SellAllHats()
+        {
+            hats.Clear();
+        }
+        public Hat GetHatWithHighestPrice()
+        {
+            return hats.OrderByDescending(hat => hat.Price).FirstOrDefault();
+        }
+        public Hat GetHatWithLowestPrice()
+        {
+            return hats.OrderBy(hat => hat.Price).FirstOrDefault();
+        }
+        public override string ToString()
+        {
+            if (hats.Count > 0)
+            {
+                return $"Hat Shop {Name} has {hats.Count} hat/s:\n" +
+                    $"{string.Join("\n", hats)}";
+            }
+            else
+            {
+                return $"Hat Shop {Name} has no available hats.";
+            }
+        }
+    }
+}
