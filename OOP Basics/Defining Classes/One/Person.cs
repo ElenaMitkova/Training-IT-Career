503fbb4901ca5f3392408391aa341ae84d3b4acd
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 20:02:24 +0200

    Add files via upload

---
+++ OOP Basics/Defining Classes/One/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Problem.Person
+{
+    class Person
+    {
+        string name;
+        int age;
+        public string Name
+        {
+            get { return this.name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        //основни понятия
+
+        public Person(string name, int age)
+        {
+            Name=name;
+            Age = age;
+        }
+        public Person()
+        {
+            Name = null;
+            Age = 0;
+        }
+        public override string ToString()
+        {
+            return $"name : {Name} , age: {Age}"; 
+        }
+        public void ChangeName(string name)
+        { 
+            //this.name = name; 
+            Name = name;
+        }
+        //public void PrintInfo ()
+        //{
+        //    Console.WriteLine($"{Name} - {Age}");
+        //}
+    }
+}
---
+++ OOP Basics/Defining Classes/One/class Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Problem.Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("Pepi",34);
+            Console.WriteLine(person2);
+            //person1.Name = "Test";
+            //Console.WriteLine(person1.Name);
+            //person1.Age = 13;
+            Console.WriteLine(person1);
+            //person1.PrintInfo();
+            //person1.name = "Ivan"; 
+            //person1.age = 12;
+            //person1.PrintInfo();
+            //Person person2;
+            //person2 = new Person();
+            //person2.name = "Ani";
+            //person2.age = 25;
+            //person2.PrintInfo();
+            //person1 = person2;
+            //Console.Write("person 1 = ");
+            //person1.PrintInfo();
+            //Console.Write("person 2 = ");
+            //person2.PrintInfo();
+
+            //person1.name = "Pesho";
+            //Console.Write("person 1 = ");
+            //person1.PrintInfo();
+            //Console.Write("person 2 = ");
+            //person2.PrintInfo();
+
+        }
+    }
+}
