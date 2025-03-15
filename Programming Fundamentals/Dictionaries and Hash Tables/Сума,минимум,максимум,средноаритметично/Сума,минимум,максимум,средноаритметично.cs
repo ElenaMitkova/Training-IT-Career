004a913519c7750e24bf907b2ffbe58dcb5f145c
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 16:10:06 +0200

    Add files via upload

---
+++ Programming Fundamentals/Dictionaries and Hash Tables/Сума,минимум,максимум,средноаритметично/Сума,минимум,максимум,средноаритметично.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine($"Sum: {array.Sum(x => x)}");
+        Console.WriteLine($"Min: {array.Min(x => x)}");
+        Console.WriteLine($"Max: {array.Max(x => x)}");
+        Console.WriteLine($"Average: {array.Average(x => x)}");
+    }
+}
---
+++ Programming Fundamentals/Dictionaries and Hash Tables/Трите_най-големи_числа/Трите_най-големи_числа.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        array = array.OrderDescending().Take(3).ToArray();
+        Console.WriteLine(string.Join(' ', array));
+    }
+}
