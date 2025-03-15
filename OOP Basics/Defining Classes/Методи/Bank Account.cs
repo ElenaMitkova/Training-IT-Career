7e50eec795d9ea69aba653497201b47c33a03301
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 20:05:23 +0200

    Add files via upload

---
+++ OOP Basics/Defining Classes/Методи/Bank Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ProblemMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.Id = int.Parse(Console.ReadLine());
+            account.Deposit(double.Parse(Console.ReadLine()));
+            account.Withdraw(double.Parse(Console.ReadLine()));
+            Console.WriteLine(account.ToString());
+        }
+    }
+}
