b7fc060399282cf577ed27ad9c241549d6984fcf
Elena Mitkova <elenamitkovaa77@gmail.com>
2025-03-15 21:08:56 +0200

    Add files via upload

---
+++ OOP Basics/Classes/Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace _01_ClassSquare
+{
+    internal class Square
+    {
+        int side;
+        public int Side 
+        { 
+            get { return side; } 
+            set { side = value;} 
+        }
+    }
+}
---
+++ OOP Basics/Classes/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using _01_ClassSquare;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+        Square square = new Square();
+        square.Side = 6;
+        Console.WriteLine($"Circumference {square.Side * 4}");
+        Console.WriteLine($"Area {square.Side * square.Side}");
+        }
+    }
